6c4b05dbc31c80a9828a1f8ff18674f679ddfb8e
james <james@192.168.1.6>
2018-10-23 12:57:10 +0200

    inicio v.1

---
+++ src/sistema/principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema;
+
+/**
+ *
+ * @author james
+ */
+public class principal {
+    public static void main(String[] args) {
+        inicio fg =new inicio();
+        fg.setVisible(true);
+    }
+}
