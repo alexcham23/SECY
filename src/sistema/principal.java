f46296b2f7b2dd895cf901fdf71810ed664c1686
james <james@192.168.1.6>
2018-11-01 18:09:42 +0100

    comit v 1.1

--- src/sistema/principal.java
+++ src/sistema/principal.java
@@ -9,9 +9,36 @@ package sistema;
  *
  * @author james
  */
+/* public void guardar(){
+     
+         
+            if(container<usuarios.length){
+                String Name ="alejandro";
+                String Lastname="armira";
+                int edad=25;
+                int Id=201602983;
+                String Rol="Administrador";
+                String Nacionalidad = "Guatemala";
+                String Username= "admin";
+                String password =String.valueOf("201602983");
+                String email="alejoarmira@gmail.com";
+                
+               // JOptionPane.showMessageDialog(this,"El Usuario se ha registrado correctamente");
+                reger = new register(Name,Lastname,edad,Id,Rol,Nacionalidad,Username,password,email);
+                usuarios[container]=reger;
+                container++;
+            }
+            
+            // esto era para ver si en realidad esta guardando
+            for(int i=0;i<container;i++){
+                 System.out.println("el rol es "+usuarios[i].getRol()+" El Usuario es "+usuarios[i].getUsername());
+            }
+           
+        
+    }*/
 public class principal {
     public static void main(String[] args) {
-        inicio fg =new inicio();
+        bienvenida fg =new bienvenida();
         fg.setVisible(true);
     }
 }
